8e35262390900baf5eb5c700e2df59570101c046
Arul Pandita <arul.pandita@gmail.com>
2026-06-08 21:33:21 -0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        left, have = 0 , 0
+        score1, score2 = defaultdict(int) , defaultdict(int)
+        for char in t:
+            score2[char] += 1
+        need, length = len(score2) , 1000
+        range_len = [len(s), len(t)]
+        for right in range(len(s)):
+            score1[s[right]] += 1 
+            if s[right] in score2 and score1[s[right]] == score2[s[right]]:
+                have += 1
+            while have == need:
+                if (right - left + 1) < length:
+                    length, range_len = right - left + 1, [left, right] 
+                score1[s[left]] -= 1 
+                if s[left] in score2 and score1[s[left]] < score2[s[left]]:                 
+                    have -= 1
+                left += 1
+        if length < 1000:
+            return s[range_len[0]:range_len[1] + 1]
+        else:
+            return ""
+             
+        
+
+                
+
+             
+
+
