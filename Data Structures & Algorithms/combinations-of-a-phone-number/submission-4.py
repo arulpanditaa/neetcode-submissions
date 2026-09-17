5e3b87e22b0ab4d18650ac2fe744ae9a7477706f
Arul Pandita <arul.pandita@gmail.com>
2026-09-16 21:58:40 -0400

    Add: combinations-of-a-phone-number - submission-4

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        naksha = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        res, sol = [], []
+
+        def btrack(n):
+            if len(sol) == len(digits):
+                res.append("".join(sol))
+                return None
+            for j in range(len(naksha[int(digits[n])])):
+                ans = naksha[int(digits[n])][j]
+                sol.append(ans)
+                btrack(n+1)
+                sol.pop()
+        if digits:
+            btrack(0)
+        return res
