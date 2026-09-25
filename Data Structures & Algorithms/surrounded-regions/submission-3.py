48bddfb3b723061ed4f1c225d1164571f247b1f6
Arul Pandita <arul.pandita@gmail.com>
2026-09-24 21:48:24 -0400

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+
+        dq, no_change = deque(), set()
+        dc = [[1,0], [-1,0], [0,1], [0,-1]]
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == "O" and (i == 0 or i ==  (len(board) - 1) or j == 0 or j == (len(board[i]) - 1)):
+                    dq.append((i,j))
+                    no_change.add((i,j))
+        while dq:
+            i, j = dq.popleft()
+            for r, c in dc:
+                row, col = i+r, j+c
+                if row in range(len(board)) and col in range(len(board[row])) and board[row][col] == "O" and (row,col) not in no_change:
+                    dq.append((row,col))
+                    no_change.add((row,col))
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if (i, j) not in no_change:
+                    board[i][j] = "X"
+
+
+
+
+            
+        
+
+        
