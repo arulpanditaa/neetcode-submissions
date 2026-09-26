0232cede0e33a615f01765ce459e5b12ef30fa61
Arul Pandita <arul.pandita@gmail.com>
2026-09-25 20:01:32 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph, seen, visited = [], set(), set()
+        for i in range(numCourses):
+            graph.append([])
+        for i, j in prerequisites:
+            graph[j].append(i)
+
+        def dfs(i):
+            if i in seen:
+                return False
+            if i in visited:
+                return True
+            seen.add(i)
+            for j in range(len(graph[i])):
+                if dfs(graph[i][j]) == False:
+                    return False
+            seen.remove(i)
+            visited.add(i)
+            return True
+
+        for i in range(numCourses):
+            if dfs(i) == False:
+                return False
+        return True
